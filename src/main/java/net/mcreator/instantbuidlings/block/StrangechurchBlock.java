42e20f7d5b0382ac40542048b2c75a166d068f67
chivas <supernunomenezes47@gmail.com>
2020-05-03 20:00:13 +0100

    first version

---
+++ src/main/java/net/mcreator/instantbuidlings/block/StrangechurchBlock.java
@@ -0,0 +1,69 @@
+
+package net.mcreator.instantbuidlings.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.instantbuidlings.procedures.Place_strangechurchProcedure;
+import net.mcreator.instantbuidlings.InstantBuidlingsElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@InstantBuidlingsElements.ModElement.Tag
+public class StrangechurchBlock extends InstantBuidlingsElements.ModElement {
+	@ObjectHolder("instantbuidlings:strangechurch")
+	public static final Block block = null;
+	public StrangechurchBlock(InstantBuidlingsElements instance) {
+		super(instance, 4);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("strangechurch");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+
+		@Override
+		public void onBlockPlacedBy(World world, BlockPos pos, BlockState state, LivingEntity entity, ItemStack itemstack) {
+			super.onBlockPlacedBy(world, pos, state, entity, itemstack);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				Place_strangechurchProcedure.executeProcedure($_dependencies);
+			}
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/instantbuidlings/procedures/PlacebarracksProcedure.java
@@ -0,0 +1,51 @@
+package net.mcreator.instantbuidlings.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.gen.feature.template.Template;
+import net.minecraft.world.gen.feature.template.PlacementSettings;
+import net.minecraft.world.World;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Mirror;
+
+import net.mcreator.instantbuidlings.InstantBuidlingsElements;
+
+@InstantBuidlingsElements.ModElement.Tag
+public class PlacebarracksProcedure extends InstantBuidlingsElements.ModElement {
+	public PlacebarracksProcedure(InstantBuidlingsElements instance) {
+		super(instance, 7);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure Placebarracks!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure Placebarracks!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure Placebarracks!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure Placebarracks!");
+			return;
+		}
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		if (!world.isRemote) {
+			Template template = ((ServerWorld) world.getWorld()).getSaveHandler().getStructureTemplateManager()
+					.getTemplateDefaulted(new ResourceLocation("instantbuidlings", "barracks"));
+			if (template != null) {
+				template.addBlocksToWorldChunk(world, new BlockPos((int) x, (int) y, (int) z),
+						new PlacementSettings().setRotation(Rotation.NONE).setMirror(Mirror.NONE).setChunk((ChunkPos) null).setIgnoreEntities(false));
+			}
+		}
+	}
+}
